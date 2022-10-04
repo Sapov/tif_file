efce4e33abf382b045e0f167bc45cd182fb5bb42
Sapov <Sapov@mail.ru>
2022-10-04 13:40:38 +0300

    1

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 resolution = {'banner': 72, 'film': 150}
 price_material = {'banner': 200, 'film': 190}  # на 28-09-2022
-yandex_TOKEN = 'y0_AgAAAAAMkDzhAAhvewAAAADPmpIrUynuQXFiRnOSaU0OBRfE1EUi1Jw'
+yandex_TOKEN = 'y0_AgBAAAAMkDzhAAhvewAAAADPmpIrUynuQXFiRnOSaU0OBRfE1EUi1Jw'
 
 propertis_material = {'banner_440': [72, 200, 'баннер ламинированный 440 грама']}
 # где
