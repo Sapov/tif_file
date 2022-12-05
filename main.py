147be6c24dbf974cd242732bb891febc742a9c20
Sapov <Sapov@mail.ru>
2022-12-05 14:16:57 +0300

    add name client

--- main.py
+++ main.py
@@ -161,6 +161,7 @@ def file_sale(file_s: str):
 
 if __name__ == "__main__":
     path_dir = str(input("Введите путь к каталогу: "))
+    client = input('Введите им клиента: ')
     # path_dir = 'C:/Users/Sasha/Downloads/замена баннеры311022'
     lst_files = list_file(path_dir)
     material = select_material()
@@ -170,7 +171,7 @@ if __name__ == "__main__":
 
     text_file_name = f'{material}_for_print_{date.today()}.txt'
     rec_to_file(text_file_name)
-    file_s = f'{material}_for_sale_{date.today()}.txt'
+    file_s = f'{client}_{material}_for_sale_{date.today()}.txt'
     file_sale(file_s)
 
     arh(lst_tif, material)  # aрхивация
