94a34286d80c0dcfdbe6c5a0039ad033f39472ae
Sapov <sapov@mail.ru>
2023-02-28 09:24:13 +0300

    edit ya_disk path

--- yandex_disk.py
+++ yandex_disk.py
@@ -21,22 +21,18 @@ def add_yadisk_locate(path):
     """закидываем файлы на yadisk локально на ubuntu"""
     Path.cwd()  # Идем в текущий каталог
     lst_files = os.listdir()  # read name files from folder
-    print(lst_files)
     for i in lst_files:
         if i.endswith("txt") or i.endswith("zip"):
-            print(f'Copy {i} files')
+            print(f'Копирую {i} в {local_path_yadisk}{path}')
             shutil.move(i, f'{local_path_yadisk}{path}')
 
 
 def add_link_from_folder_yadisk(path):
-    print(f'Опубликовал папку {path}')
-    # ya_link = os.system(f"yandex-disk publish {path}")
+    print(f'Опубликовал папку {local_path_yadisk}{path}')
     ya_link = subprocess.check_output(["yandex-disk", "publish", f'{local_path_yadisk}{path}'])
-
-    print(f' Получил {ya_link}')
-    print('Type: ', type(ya_link))
     ya_link = str(ya_link)
-    ya_link = ya_link.lstrip("b'").rstrip("\n'")
-    print('Type: ', type(ya_link))
+    ya_link = ya_link.lstrip("b'")
+    ya_link = ya_link.rstrip("\n'")
+    print(f'Ссылка на яндекс диск {ya_link}')
 
     return ya_link
