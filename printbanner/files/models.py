c9234a6845cffc8f3056ac93f0a9e9c0ff4b241e
Sapov <sapov@mail.ru>
2023-02-28 20:54:41 +0300

    add django01

--- printbanner/files/models.py
+++ printbanner/files/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Files(models.Model):
+    title = models.CharField(max_length=150)
+    cover = models.ImageField(upload_to='images/')
+    book = models.FileField(upload_to='books/')
+
+    def __str__(self):
+        return self.title
--- printbanner/files/views.py
+++ printbanner/files/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Files
 
-# Create your views here.
+
+# def home_page(request):
+#     # получаем все значения модели
+#     data = Files.objects.all()
+#     return render(request, 'home_page.html', {'data': data})
+
+from django.core.files.storage import FileSystemStorage
+
+def home_page(request):
+    # POST - обязательный метод
+    if request.method == 'POST' and request.FILES:
+        # получаем загруженный файл
+        file = request.FILES['myfile1']
+        fs = FileSystemStorage()
+        # сохраняем на файловой системе
+        filename = fs.save(file.name, file)
+        # получение адреса по которому лежит файл
+        file_url = fs.url(filename)
+        Files.objects.create(title='some title', cover=request.FILES['myfile1'])
+
+        return render(request, 'home_page.html', {
+            'file_url': file_url
+        })
+    return render(request, 'home_page.html')
